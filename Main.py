dd9a61131cc54336be68e79cb3982d0a413247f3
Krhuhun <72083150+jorgelisboa@users.noreply.github.com>
2022-05-19 21:54:14 -0300

    anotações maligno

--- Main.py
+++ Main.py
@@ -1,3 +1,68 @@
+# função auxiliar  recursiva que, de fato, gera as permutacoes
+# NÃO USE DIRETAMENTE ESTA FUNÇÃO; USE A FUNÇÃO permutacoes
+# recebe uma lista com os valores a serem permutados (linha),
+# uma lista com os itens na permutação sendo gerada (perm) e
+# uma lista com as permutações geradas (perms) 
+def permuta (linha, perm, perms):
+    if linha==[]:
+        perms.append(perm)
+    else:
+        for lin in range(len(linha)):
+            permuta(linha[0:lin]+linha[lin+1:len(linha)],perm+[linha[lin]],perms)
+
+# função principal para gerar permutações;
+# USA A FUNÇÃO permuta, QUE NÃO DEVE SER USADA DIRETAMENTE;
+# recebe uma lista com os valores a serem permutados (linha)
+# retorna as permutações geradas
+def permutacoes (linha):
+    perms=[]
+    permuta(linha,[],perms)
+    return perms
+    
+def haZeroNaDiagonal (m,ordL,ordC):
+    qtdDeZeros=0
+    posicao=0
+    while posicao<len(m):
+        if m[ordL[posicao]][ordC[posicao]]==0: qtdDeZeros+=1
+        posicao+=1
+    return qtdDeZeros>0
+   
+def comoSeLivrarDeZerosNaDiagonal (m):
+    perms = permutacoes(list(range(len(m))))
+
+    for i in range(len(perms)):
+        for j in range(len(perms)):
+            if not haZeroNaDiagonal(m,perms[i],perms[j]):
+                return [perms[i],perms[j]];
+
+# "Divide" os pares de linha - (Já tá fazendo os pares, falta dividor)
+
+def isMatrizValida(matriz):
+    #todas as linhas com a mesma quantidade de coluna  
+    currentLine = 0
+    compareLine = 0
+    currentColumn = 0
+    while currentLine < len(matriz): #percorre as linhas
+        compareLine = currentLine + 1
+        while compareLine < len(matriz):
+            currentColumn = 0
+            while currentColumn < (len(matriz[currentLine])-1): # percorre coluna da linha atual
+                if (currentLine < (len(matriz)-1)):
+                    print('repedição: ', currentLine, ' linha ', matriz[currentLine][currentColumn],'linha', compareLine, " ", matriz[compareLine][currentColumn])
+                currentColumn +=1
+            compareLine +=1
+        currentLine +=1
+
+
+##################################################################################################
+
+
+
+
+
+
+
+
 # Our matriz
 matriz = [[2,3,5,1], [9,2,6,7], [3,6,2,9]]
 
